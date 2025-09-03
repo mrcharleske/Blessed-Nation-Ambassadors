7163b747d6d761c9605ffeaa75f1179c6f88b572
mrcharleske <njorogecharles5460@gmail.com>
2025-09-03 11:28:37 +0300

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-// Basic hover effects and animations are handled in CSS; add JS if needed for more (e.g., smooth scroll)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
